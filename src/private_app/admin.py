9f06fae19089cf1098cb5cc449354322406f96e6
Gilles Helleu <helleugilles@gmail.com>
2024-01-12 12:28:26 +0100

    add UserProfileAdmin adn LinkInLine

--- src/private_app/admin.py
+++ src/private_app/admin.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 
 # Local imports
-from .models import Address
+from .models import Address, UserProfile
 
 
 class AddressAdmin(admin.ModelAdmin):
@@ -16,4 +16,18 @@ class AddressAdmin(admin.ModelAdmin):
     search_fields = ("postcode", "locality")
 
 
+class LinkInline(admin.TabularInline):
+    model = Link
+    extra = 1
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('id', "user", "image_profile",
+                    "url_tiktok", "url_instagram", "about_me", "is_public", "student_at")
+    list_filter = ("user", "student_at", "is_public",)
+    search_fields = ("user", "student_at", "about_me")
+    inlines = [LinkInline]
+
+
 admin.site.register(Address, AddressAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
