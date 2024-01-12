3ba2a4cfd1cc7ac89a37899c3fabb5281123af62
Gilles Helleu <helleugilles@gmail.com>
2024-01-12 17:15:12 +0100

    change settings

--- src/future_academie/settings.py
+++ src/future_academie/settings.py
@@ -60,7 +60,17 @@ INSTALLED_APPS = [
     "private_app",
     "public_app",
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
 
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
