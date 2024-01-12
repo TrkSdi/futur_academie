e62417bc7e741c936867e2ffccfe46f28195b60e
Rachel W. <rachelwilliams@mailbox.org>
2024-01-12 17:37:28 +0100

    added filters

--- src/private_app/views/address.py
+++ src/private_app/views/address.py
@@ -1,4 +1,5 @@
 # Third-party imports
+from django_filters import rest_framework as filters
 from rest_framework import serializers, viewsets
 
 # Local imports
@@ -11,6 +12,18 @@ class AddressSerializer(serializers.ModelSerializer):
         fields = ["street_address", "postcode", "locality", "geolocation"]
 
 
+class AddressFilter(filters.FilterSet):
+    class Meta:
+        model = Address
+        fields = {
+            "street_address": ["icontains"],
+            "postcode": ["contains", "exact"],
+            "locality": ["icontains", "exact"],
+        }
+
+
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+    filterset_class = AddressFilter
+    filter_backends = (filters.DjangoFilterBackend,)
--- src/private_app/views/school.py
+++ src/private_app/views/school.py
@@ -1,16 +1,40 @@
 # Third-party imports
+from django_filters import rest_framework as filters
 from rest_framework import serializers, viewsets
 
 # Local imports
 from private_app.models import School
-    
-    
+
+
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
-        fields = ['UAI_code','name', 'school_url', 'description', 'address', 'school_type']
-        
+        fields = [
+            "UAI_code",
+            "name",
+            "school_url",
+            "description",
+            "address",
+            "school_type",
+        ]
+
+
+class SchoolFilter(filters.FilterSet):
+    # add possibility to filter by address
+    class Meta:
+        model = School
+        fields = {
+            "UAI_code": ["exact"],
+            "name": ["icontains", "exact"],
+            "description": ["icontains"],
+            "school_type": ["exact"],
+        }
+
 
 class SchoolViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
+    filterset_class = SchoolFilter
+    filter_backends = [
+        filters.DjangoFilterBackend,
+    ]
