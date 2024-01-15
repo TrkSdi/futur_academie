14e70b575f44eb209fee7c32aa33400fe2bda176
Damien Vialla de Soleyrol <damienvialla@gmail.com>
2024-01-15 11:45:36 +0100

    filtre favorite

--- src/private_app/views/favorite.py
+++ src/private_app/views/favorite.py
@@ -1,5 +1,6 @@
 # Third-party imports
 from rest_framework import serializers, viewsets
+from django_filters import rest_framework as filters
 
 # Local imports
 from private_app.models import Favorite
@@ -12,7 +13,23 @@ class FavoriteSerializer(serializers.ModelSerializer):
 
         fields = ["id", "user", "study_program", "note", "status"]
 
+class FavoriteFilter(filters.FilterSet):
+    # add possibility to filter by address
+    class Meta:
+        model = Favorite
+        fields = {
+            "UAI_code": ["exact"],
+            "name": ["icontains", "exact"],
+            "description": ["icontains"],
+            "school_type": ["exact"],
+        }
 
 class FavoriteViewSet(viewsets.ModelViewSet):
     queryset = Favorite.objects.all()
     serializer_class = FavoriteSerializer
+    filterset_class = FavoriteFilter
+    filter_backends = [
+        filters.DjangoFilterBackend,
+    ]
+    
+
