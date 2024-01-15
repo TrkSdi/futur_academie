b48ac24e94dce970ac0ad3fe8272e93b6294e6b3
Gilles Helleu <helleugilles@gmail.com>
2024-01-15 14:33:49 +0100

    add filters to User Profile

--- src/private_app/views/userprofile.py
+++ src/private_app/views/userprofile.py
@@ -1,5 +1,7 @@
 # Third-party imports
 from rest_framework import serializers, viewsets
+from django_filters import rest_framework as filters
+
 
 # Local imports
 from private_app.models import UserProfile, User
@@ -10,6 +12,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email"]
+
+
+class UserProfileFilter(filters.FilterSet):
+    # user_first_name = filters.CharFilter(
+    #     field_name="first_name", lookup_expr="icontains"
+    # )
+
+    class Meta:
+        model = UserProfile
+        fields = {"user__first_name": ["icontains"],
+                  "user__last_name": ["icontains", "exact"],
+                  "about_me": ["icontains"],
+                  "student_at__name": ["icontains"],
+                  "student_at__cod_aff_form": ["exact"]
+                  }
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -36,3 +53,5 @@ class UserProfileSerializer(serializers.ModelSerializer):
 class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+    filterset_class = UserProfileFilter
+    filter_backends = (filters.DjangoFilterBackend,)
