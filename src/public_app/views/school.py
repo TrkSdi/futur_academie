25a8078f780a32d48c0943287221fc2b67701443
Rachel W. <rachelwilliams@mailbox.org>
2024-01-17 11:54:47 +0100

    renamed distance filter to make usage clearer

--- src/public_app/views/school.py
+++ src/public_app/views/school.py
@@ -41,7 +41,7 @@ class SchoolSerializerPublic(serializers.ModelSerializer):
 
 class SchoolFilterPublic(filters.FilterSet):
     # Returns all results ordered by distance from the point given in format long,lat
-    distance = filters.CharFilter(method="filter_by_distance")
+    distance_from = filters.CharFilter(method="filter_by_distance")
     # Returns all results within the radius in km of the point given in format long,lat,radius
     distance__lte = filters.CharFilter(method="filter_distance_lte")
 
