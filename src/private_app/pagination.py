083f4b0ff4c6c7a26e820a56921ae5cc8cd1de0b
Rachel W. <rachelwilliams@mailbox.org>
2024-02-05 11:54:16 +0100

    increased pagination size

--- src/private_app/pagination.py
+++ src/private_app/pagination.py
@@ -5,6 +5,6 @@ class MyPagination(pagination.PageNumberPagination):
     """Class for setting default and max pagination for the public
     API."""
 
-    page_size = 5
+    page_size = 6
     max_page_size = 1000
     page_size_query_param = "page_size"
