6c69e800dfecd01361f18ecb9d1753644ff019c5
Rachel W. <rachelwilliams@mailbox.org>
2024-02-06 15:22:37 +0100

    automatic user profile creation

--- src/private_app/apps.py
+++ src/private_app/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class PrivateAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'private_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "private_app"
+
+    def ready(self):
+        import private_app.signals
