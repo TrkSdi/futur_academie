a43a74fced80c2d2bb2d91433b6d1833eda9d26d
Gilles Helleu <helleugilles@gmail.com>
2024-02-09 12:47:46 +0100

    change url in activation

--- src/private_app/views/accounts/activation.py
+++ src/private_app/views/accounts/activation.py
@@ -18,7 +18,8 @@ class ActivateUser(UserViewSet):
         serializer_class = self.get_serializer_class()
         kwargs.setdefault("context", self.get_serializer_context())
 
-        kwargs["data"] = {"uid": self.kwargs["uid"], "token": self.kwargs["token"]}
+        kwargs["data"] = {"uid": self.kwargs["uid"],
+                          "token": self.kwargs["token"]}
 
         return serializer_class(*args, **kwargs)
 
@@ -26,5 +27,5 @@ class ActivateUser(UserViewSet):
         response = super().activation(request, *args, **kwargs)
 
         if response.status_code == status.HTTP_204_NO_CONTENT:
-            return redirect("http://localhost:4200/login")
+            return redirect("http://152.228.219.128/login")
         return response
